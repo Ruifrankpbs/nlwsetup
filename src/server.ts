86a9d68a068e1f2a4a44102aede306096b5f2626
Rui Franke <ruifrankpbs@gmail.com>
2023-01-18 01:27:26 -0300

    Back-end´s setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import Fastifay from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
 
 const app = Fastifay()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-    return 'Hello Mundo!'
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title:{
+                startsWith: 'Aprender'
+            }
+        }
+    })
+    return habits
 })
 
 app.listen({
